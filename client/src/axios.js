4782f4c78648305592bcbe80fa42f7cec69ba5fd
delucia <oguzhankukul@gmail.com>
2023-01-21 20:31:33 -0600

    client side ready

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://todo-4qbe1f1bf-oguzhan76.vercel.app'
+    baseURL: process.env.API_URL
 });
 
 export default axiosInstance;
